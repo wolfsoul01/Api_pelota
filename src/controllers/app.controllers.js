bbf92f5c1eb27058db3fc78df7c439d0aba750b4
wolfsoul <alejandrof011021@gmail.com>
2023-10-10 00:20:05 -0400

    integracion con la db

--- src/controllers/app.controllers.js
+++ src/controllers/app.controllers.js
@@ -1,9 +1,34 @@
+import mysql2 from 'mysql2'
+
+const config= {
+    host:'127.0.0.2',
+    user:'root',
+    password:'admin',
+    database:'mydb'
+}
+
+const conection = mysql2.createConnection(config)
+
 
 
 export class appControllers{
+ 
+    static async appGet (req,res){
 
-    static async appGet(req,res){
+        conection.connect(err => {
 
-        res.send('Api')
+            if(err) {
+              console.error(err);
+              return res.status(500).json({msg: 'Error de conexión a DB'}); 
+            } 
+
+          });
+
+           conection.query(`SELECT * FROM juegos
+         `,(err,data)=>{
+            console.log(data);
+            res.json(data)
+         })
+
     }
 }
