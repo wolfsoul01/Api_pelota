10e7cf47b4736a62e42b2212a4c84294b34544fb
wolfsoul <alejandrof011021@gmail.com>
2023-10-18 14:57:41 -0400

    Create new pool conection

--- src/models/equipos.model.js
+++ src/models/equipos.model.js
@@ -3,7 +3,6 @@ import { connection } from '../utils/connectionDB.js'
 export class EquipoModel {
   static async getAll () {
     const [data] = await connection.query('SELECT * FROM equipo')
-    console.log(data)
     return data
   }
 
--- src/utils/connectionDB.js
+++ src/utils/connectionDB.js
@@ -1,10 +1,21 @@
 import mysql2 from 'mysql2/promise'
 
-const config = {
+// const config = {
+//   host: '127.0.0.2',
+//   user: 'root',
+//   password: 'admin',
+//   database: 'mydb'
+// }
+
+// export const connection = await mysql2.createConnection(config)
+
+export const connection = await mysql2.createPool({
   host: '127.0.0.2',
   user: 'root',
   password: 'admin',
-  database: 'mydb'
-}
-
-export const connection = await mysql2.createConnection(config)
+  database: 'mydb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  idleTimeout: 60000,
+  enableKeepAlive: true
+})
